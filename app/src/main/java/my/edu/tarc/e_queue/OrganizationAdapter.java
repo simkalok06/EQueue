dc62b521178924200464bec0b3dc3339d7ceea55
Joel Sim Ka Lok <simkl-wa16@student.tarc.edu.my>
2018-12-23 16:54:48 +0800

    version 8 commit - Need to make Organization as a list

---
+++ app/src/main/java/my/edu/tarc/e_queue/OrganizationAdapter.java
@@ -0,0 +1,17 @@
+package my.edu.tarc.e_queue;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class OrganizationAdapter {
+
+
+
+
+}
